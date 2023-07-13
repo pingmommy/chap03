f8e496e08e23b77e870c0afab8abc613c04041f1
mcar8764 <mcar8764@gmail.com>
2023-07-13 18:18:20 +0900

    add Line about month,day,hour but Need to Modifiy

--- src/EpochTimeExample.java
+++ src/EpochTimeExample.java
@@ -4,7 +4,7 @@ public class EpochTimeExample {
 	public static void main(String[] args) {  //>>초단위(/1000)로 바꿔라.
 		long epoch = System.currentTimeMillis()/1000+9*60*60;  
 		                                          //>>9*60*60 우리나라는 9시간을 더해야 하고
-		System.out.println(epoch);                // 단위를 맞추기 위해 초단위로 환산. 
+		//System.out.println(epoch);                // 단위를 맞추기 위해 초단위로 환산. 
 		
 		int year = 1970;
 		for (;;) {
@@ -20,18 +20,41 @@ public class EpochTimeExample {
 		   break;
 		}
 		
-		System.out.println(year);
-		System.out.println(epoch); 
+		//31일 월 - 상반기는 홀수월(1,3,5,7)에, 하반기는 짝수월(8,10,12)에 더해준다.
 		
-		long mm = epoch/60/60L;
+		System.out.print(year+"년");
+		//System.out.println(epoch); 
 		
-		long dd = mm/24L;
+		long minute = epoch/60L;
+		//System.out.println("minute = "+ minute);
 		
-		int mon = (int)dd/30;
+		long hour = minute/60L;
+		//System.out.println("hour = "+ hour);
+		
+		long day = hour/24;
+		//System.out.println("day = "+ day);
+		
+		int month = (int)day/30; // 30/ 31 /2월 28
+		//System.out.println("month = "+ month);
+		
+		int left = (int)day%30;
+		//System.out.println("left = "+ left);
+		
+		int todayHour = (int)day/30;
+		
+		System.out.printf("%d월%d일",month+1,left);
+		System.out.println(todayHour+"시");
+
+
+
+
+
+		
+		//int mon = (int)dd/30;
 		
 		
 		
-		System.out.println(mon);
+		//System.out.println(mon);
 	}
 
 }
