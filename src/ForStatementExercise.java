c48a05d38b91646919750bf69ecd357c0dfb5142
mcar8764 <mcar8764@gmail.com>
2023-10-25 09:39:30 +0900

    ..

--- src/ForStatementExercise.java
+++ src/ForStatementExercise.java
@@ -5,11 +5,11 @@ public class ForStatementExercise {
 	
 		 
 
- public static void main1(String[] args) {
+ public static void main(String[] args) {
 	 int sum=0; 
 	 int i;
 	 
-	 for(i=1;sum<=100; i++) {
+	 for(i=1;sum<100; i++) {
 	     System.out.println("i="+i);
 
 		 if(i%2==0) {
